5f021ff554f2c8474e7401547f00f2e656c5a097
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-04-15 17:07:55 +0200

    Complete initial setup and display functionality

--- challenges/programmer-humour/scritp.js
+++ challenges/programmer-humour/scritp.js
@@ -1,6 +1,17 @@
 let apiAddress = `https://xkcd.now.sh/?comic=latest`;
 
+const container = document.getElementById("content");
+
+function displayComic(imgUrl){
+    let newImg = document.createElement("img");
+    container.appendChild(newImg);
+    newImg.src = imgUrl;
+}
+
 fetch(apiAddress)
 .then(response => response.json())
-.then(data => console.log(data.img))
+.then(data => {
+    console.log(data)
+    displayComic(data.img);
+})
 .catch(error => console.log(error));
