9ca86e633311bd659189add6943be0524c9b7a61
RyanS-Cpt <74462713+RyanS-Cpt@users.noreply.github.com>
2021-04-15 19:47:05 +0200

    Complete programmer humour

--- challenges/programmer-humour/scritp.js
+++ challenges/programmer-humour/scritp.js
@@ -2,16 +2,26 @@ let apiAddress = `https://xkcd.now.sh/?comic=latest`;
 
 const container = document.getElementById("content");
 
-function displayComic(imgUrl){
+function displayComic(dataObj){
     let newImg = document.createElement("img");
+    let newHeading = document.createElement("h1");
+    container.appendChild(newHeading);
+    newHeading.innerText = dataObj.safe_title;
     container.appendChild(newImg);
-    newImg.src = imgUrl;
+    newImg.src = dataObj.img;
 }
 
 fetch(apiAddress)
-.then(response => response.json())
+.then(response => {
+    console.log(response);
+    if (response.status >= 200 && response.status < 300){
+    return response.json();
+    }else{
+        throw `Error here! ${response.status}:${response.statusText}`;
+    }
+})
 .then(data => {
     console.log(data)
-    displayComic(data.img);
+    displayComic(data);
 })
-.catch(error => console.log(error));
+.catch(error => alert(error));
